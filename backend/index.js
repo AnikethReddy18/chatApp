4988be900d7774923f13054a94a5ff9e4548e7ca
aniketh <sunnyrockzzz1811@gmail.com>
2025-02-06 14:15:09 +0530

    Add CORS

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express, { urlencoded } from "express"
 import router from "./routers/router.js"
+import cors from "cors"
 
 const app = express();
 const PORT = process.env.PORT || 3000
 app.use(urlencoded({extended: true}))
+app.use(cors())
 
 app.use("/", router.authRouter);
 app.use("/", router.messageRouter);
