8de8c4aa718ff673b671c5de7377fa0045a8fde6
aniketh <sunnyrockzzz1811@gmail.com>
2025-02-12 07:17:03 +0530

    Implement Get Messages In Frontend

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,9 +1,32 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import apiClient from "../apiClient"
 
 function Chat() {
   const { username } = useParams();
+  const [messages, setMessages] = useState([])
 
-  return <div>Chatting with {username}</div>;
+  useEffect(()=>{
+    async function getMessages(){
+      const userResponse = await apiClient.get("/user/"+username)
+      const receiver_id = userResponse.data.id
+
+      const response = await apiClient.get("/message", {
+        headers: {Authorization: "Bearer " + localStorage.getItem('token')},
+        params: {receiver_id}
+      } )
+      console.log(response.data)
+    }
+
+    getMessages()
+  },[])
+
+  return (
+  <>
+  <div>Chatting with {username}</div>
+
+
+  </>);
 }
 
 export default Chat
